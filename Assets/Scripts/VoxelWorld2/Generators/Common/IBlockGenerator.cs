ba5b286c447f278889c5916ed896ef18a88b2ea6
Maxime Giguere <maximeetiennegiguere@gmail.com>
2020-11-15 17:07:03 -0500

    Remove old implementations

--- Assets/Scripts/VoxelWorld2/Generators/Common/IBlockGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace VoxelWorld2.Generators.Common
-{
-	public interface IBlockGenerator
-	{
-		void Generate(Vector3Int offset, out IBlockGeneratorResult result);
-	}
-}
